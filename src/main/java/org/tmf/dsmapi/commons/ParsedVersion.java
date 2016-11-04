93c350f3ed4b3a82046d111d0be4a9cdeb1aaba1
fdelavega <fdelavega@conwet.com>
2016-11-04 17:10:22 +0100

    Add support to versions with more than two fields

--- src/main/java/org/tmf/dsmapi/commons/ParsedVersion.java
+++ src/main/java/org/tmf/dsmapi/commons/ParsedVersion.java
@@ -2,10 +2,15 @@ package org.tmf.dsmapi.commons;
 
 import java.io.Serializable;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *
- * @author bahman.barzideh
+ * @author bahman.barzideh, fdelavega
  *
  */
 public class ParsedVersion implements Serializable {
@@ -20,7 +25,7 @@ public class ParsedVersion implements Serializable {
     private static final DecimalFormat minorVersionInternalFormat = new DecimalFormat("000");
 
     private Integer majorVersion;
-    private Integer minorVersion;
+    private List<Integer> minorVersions;
 
     private String externalView;
     private String internalView;
@@ -29,17 +34,17 @@ public class ParsedVersion implements Serializable {
 
     private ParsedVersion(String internalView, String externalView) {
         this.majorVersion = null;
-        this.minorVersion = null;
+        this.minorVersions = new ArrayList<>();
 
         this.externalView = externalView;
         this.internalView = internalView;
 
-        valid = true;
+        this.valid = true;
     }
 
     public ParsedVersion(String version) throws IllegalArgumentException {
         initialize_();
-        if (load_(version) == false) {
+        if (!load_(version)) {
             throw new IllegalArgumentException("'" + version + "' is not a valid version.");
         }
 
@@ -47,34 +52,87 @@ public class ParsedVersion implements Serializable {
             throw new IllegalArgumentException ("Major version, " + majorVersion + ", is too large; maximum value=" + MAX_MAJOR_VERSION);
         }
 
-        if (minorVersion > MAX_MINOR_VERSION) {
-            throw new IllegalArgumentException ("Minor version, " + minorVersion + ", is too large; maximum value=" + MAX_MINOR_VERSION);
+        List<Integer> invMinors = this.minorVersions.stream()
+                .filter((minorVersion) -> minorVersion > MAX_MINOR_VERSION)
+                .collect(Collectors.toList());
+
+        if (invMinors.size() > 0) {
+            String errMinors = "";
+            invMinors.stream().map((minor) -> "," + minor.toString()).reduce(errMinors, String::concat);
+
+            throw new IllegalArgumentException ("Minor versions, " + errMinors + ", are too large; maximum value=" + MAX_MINOR_VERSION);
         }
 
         this.externalView = createExternalView_();
         this.internalView = createInternalView_();
 
-        valid = true;
+        this.valid = true;
+    }
+
+    public String getExternalView() {
+        return this.externalView;
+    }
+
+    public String getInternalView() {
+        return this.internalView;
     }
 
     public Integer getMajorVersion() {
         return majorVersion;
     }
 
-    public Integer getMinorVersion() {
-        return minorVersion;
-    }
-
-    public String getExternalView() {
-        return externalView;
-    }
-
-    public String getInternalView() {
-        return internalView;
+    public List<Integer> getMinorVersions() {
+        return minorVersions;
     }
 
     public boolean isValid() {
-        return valid;
+        return this.valid;
+    }
+
+    public boolean isGreaterThan(ParsedVersion other) {
+        if (!this.isValid()) {
+            throw new IllegalArgumentException ("invalid version object");
+        }
+
+        if (other == null || !other.isValid()) {
+            throw new IllegalArgumentException ("invalid other version object");
+        }
+
+        if (this == ROOT_CATALOG_VERSION) {
+            return false;
+        }
+
+        if (other == ROOT_CATALOG_VERSION) {
+            return true;
+        }
+
+        // Compare major version
+        int compare = this.majorVersion.compareTo(other.getMajorVersion());
+        if (compare < 0) {
+            return false;
+        }
+
+        if (compare > 0) {
+            return true;
+        }
+
+        // Compare minor versions
+        int minorCompare = 0;
+        Iterator<Integer> localIt = this.minorVersions.iterator();
+        Iterator<Integer> otherIt = other.getMinorVersions().iterator();
+
+        while(minorCompare == 0 && localIt.hasNext() && otherIt.hasNext()) {
+            minorCompare = localIt.next().compareTo(otherIt.next());
+
+            // Version may have different number of minor versions
+            if (minorCompare == 0 && !otherIt.hasNext()) {
+                minorCompare = 1;
+            } else if (minorCompare == 0 && !localIt.hasNext()) {
+                minorCompare = -1;
+            }
+        }
+
+        return minorCompare > 0;
     }
 
     @Override
@@ -82,7 +140,15 @@ public class ParsedVersion implements Serializable {
         int hash = 7;
 
         hash = 89 * hash + (this.majorVersion != null ? this.majorVersion.hashCode() : 0);
-        hash = 89 * hash + (this.minorVersion != null ? this.minorVersion.hashCode() : 0);
+
+        if (this.minorVersions.size() > 0) {
+            for (Integer minorVersion: this.minorVersions) {
+                hash = 89 * hash + minorVersion.hashCode();
+            }
+        } else {
+            hash = 89 * hash + 0;
+        }
+
         hash = 89 * hash + (this.externalView != null ? this.externalView.hashCode() : 0);
         hash = 89 * hash + (this.internalView != null ? this.internalView.hashCode() : 0);
         hash = 89 * hash + (this.valid == true ? 1 : 0);
@@ -97,69 +163,64 @@ public class ParsedVersion implements Serializable {
         }
 
         final ParsedVersion other = (ParsedVersion) object;
-        if (Utilities.areEqual(this.majorVersion, other.majorVersion) == false) {
+
+        if(this.valid != other.isValid()) {
             return false;
         }
 
-        if (Utilities.areEqual(this.minorVersion, other.minorVersion) == false) {
+        if (!Utilities.areEqual(this.majorVersion, other.getMajorVersion())) {
             return false;
         }
 
-        if (Utilities.areEqual(this.externalView, other.externalView) == false) {
+        if (!Utilities.areEqual(this.externalView, other.getExternalView())) {
             return false;
         }
 
-        if (Utilities.areEqual(this.internalView, other.internalView) == false) {
+        if (!Utilities.areEqual(this.internalView, other.getInternalView())) {
             return false;
         }
 
-        if (this.valid == other.valid) {
+        if (this.minorVersions.size() != other.getMinorVersions().size()) {
             return false;
         }
 
-        return true;
+        boolean result = true;
+        int i = 0;
+        while(result && i < this.minorVersions.size()) {
+            if (!Utilities.areEqual(this.minorVersions.get(i), other.getMinorVersions().get(i))) {
+                result = false;
+            }
+        }
+
+        return result;
     }
 
     @Override
     public String toString() {
+        String minorVersion = "";
+            this.minorVersions.stream().map((minor) -> "," + minor.toString())
+                    .reduce(minorVersion, String::concat);
+
         return "ParsedVersion{" + "majorVersion=" + majorVersion + ", minorVersion=" + minorVersion + ", externalView=" + externalView + ", internalView=" + internalView + ", valid=" + valid + '}';
     }
 
-    public boolean isGreaterThan(ParsedVersion other) {
-        if (this.isValid() == false) {
-            throw new IllegalArgumentException ("invalid version object");
-        }
-
-        if (other == null || other.isValid() == false) {
-            throw new IllegalArgumentException ("invalid other version object");
-        }
-
-        if (this == ROOT_CATALOG_VERSION) {
-            return false;
-        }
-
-        if (other == ROOT_CATALOG_VERSION) {
-            return true;
-        }
-
-        int compare = this.majorVersion.compareTo(other.majorVersion);
-        if (compare < 0) {
-            return false;
-        }
-
-        if (compare > 0) {
-            return true;
-        }
-
-        return (this.minorVersion.compareTo (other.minorVersion) > 0) ? true : false;
-    }
-
     private String createInternalView_() {
-        return (majorVersionInternalFormat.format(majorVersion) + "." + minorVersionInternalFormat.format(minorVersion));
+        String view = majorVersionInternalFormat.format(majorVersion);
+
+        view = this.minorVersions.stream().map((minorVersion) -> 
+                "." + minorVersionInternalFormat.format(minorVersion))
+                .reduce(view, String::concat);
+
+        return view;
     }
 
     private String createExternalView_() {
-        return (majorVersion + "." + minorVersion);
+        String view = this.majorVersion.toString();
+
+        view = this.minorVersions.stream().map((minorVersion) -> 
+                "." + minorVersion.toString()).reduce(view, String::concat);
+
+        return view;
     }
 
     private boolean load_(String input) {
@@ -167,23 +228,26 @@ public class ParsedVersion implements Serializable {
             return false;
         }
 
+        // Check that the string is not starting or ending by .
+        if (input.startsWith(".") || input.endsWith(".")) {
+            return false;
+        }
+
         String parts [] = input.split ("\\.");
-        if (parts.length != 2) {
+        if (parts.length < 2) {
             return false;
         }
 
         try {
-            majorVersion = Integer.parseInt(parts [0]);
-        }
-        catch (Exception ex) {
-            return false;
-        }
+            this.majorVersion = Integer.parseInt(parts [0]);
+            List<String> minorParts = new ArrayList<>(Arrays.asList(parts));
+            minorParts.remove(0);
 
-        try {
-            minorVersion = Integer.parseInt(parts [1]);
-        }
-        catch (Exception ex) {
-            initialize_();
+            this.minorVersions.addAll(minorParts.stream().map((minor) -> {
+                return Integer.parseInt(minor);
+            }).collect(Collectors.toList()));
+
+        } catch (Exception ex) {
             return false;
         }
 
@@ -191,13 +255,13 @@ public class ParsedVersion implements Serializable {
     }
 
     private void initialize_() {
-        majorVersion = null;
-        minorVersion = null;
+        this.majorVersion = null;
+        this.minorVersions = new ArrayList<>();
 
-        externalView = null;
-        internalView = null;
+        this.externalView = null;
+        this.internalView = null;
 
-        valid = false;
+        this.valid = false;
     }
 
 }
